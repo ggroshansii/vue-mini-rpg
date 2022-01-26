7734c27332646a0ba9491cd54566b68f82f57e51
ggroshansii <garthgroshansii@gmail.com>
2022-01-26 11:20:11 -0500

    created special attack function for click event

--- app.js
+++ app.js
@@ -17,8 +17,28 @@ new Vue({
             this.playerHealth = this.playerHealth - monsterDamage;
             this.monsterHealth = this.monsterHealth - playerDamage;
             
-            this.attackHistory.push(`Player attacks Monster for ${playerDamage} amount`)
-            this.attackHistory.push(`Monster attacks Player for ${monsterDamage} amount`)
+            this.attackHistory.push(`Player attacks Monster for ${playerDamage}`)
+            this.attackHistory.push(`Monster attacks Player for ${monsterDamage}`)
+        },
+        specialAttack: function() {
+            let playerDamage = Math.floor((Math.random() * 30) + 1)
+            let monsterDamage = Math.floor((Math.random() * 10) + 1)
+            
+            this.playerHealth = this.playerHealth - monsterDamage;
+            this.monsterHealth = this.monsterHealth - playerDamage;
+            
+            this.attackHistory.push(`Player attacks Monster for ${playerDamage}`)
+            this.attackHistory.push(`Monster attacks Player for ${monsterDamage}`)
+        },
+        heal: function() {
+            let playerHeal = Math.floor((Math.random() * 15) + 1)
+            let monsterDamage = Math.floor((Math.random() * 10) + 1)
+            
+            this.playerHealth = this.playerHealth + playerHeal;
+            this.monsterHealth = this.monsterHealth - playerDamage;
+            
+            this.attackHistory.push(`Player heals for ${playerHeal}`)
+            this.attackHistory.push(`Monster attacks Player for ${monsterDamage}`)
         }
     }
 })
