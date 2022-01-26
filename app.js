3bc33e50f7696467542866c6bc3b17b0ed70e84a
ggroshansii <garthgroshansii@gmail.com>
2022-01-26 10:51:24 -0500

    health bars now dynamically change to player/monster health status

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ new Vue({
     el: '#app',
     data: {
         gameOn: false,
-
+        playerHealth: 100,
+        monsterHealth: 100,
+        // healthBarStyle: {
+        //     backgroundColor: 'green',
+        //     margin: 0,
+        //     color: 'white',
+        // }
     },
     methods: {
         startGame: function() {
             this.gameOn = true;
         },
         attack: function() {
-
+            let playerDamage = Math.floor((Math.random() * 10) + 1)
+            let monsterDamage = Math.floor((Math.random() * 10) + 1)
+            
+            this.playerHealth = this.playerHealth - monsterDamage;
+            this.monsterHealth = this.monsterHealth - playerDamage;
         }
     }
 })
