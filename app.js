84ed5d5c8280f4234eea2e6e43018a132e35c254
ggroshansii <garthgroshansii@gmail.com>
2022-01-26 11:43:34 -0500

    added watch properties to alert when either player's or monster's health goes below zero

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
 new Vue({
-    el: '#app',
-    data: {
-        gameOn: false,
-        playerHealth: 100,
-        monsterHealth: 100,
-        attackHistory: [],
+  el: "#app",
+  data: {
+    gameOn: false,
+    playerHealth: 100,
+    monsterHealth: 100,
+    attackHistory: [],
+  },
+  methods: {
+    startGame: function () {
+      this.gameOn = true;
     },
-    methods: {
-        startGame: function() {
-            this.gameOn = true;
-        },
-        selectionPick: function(event) {
+    selectionPick: function (event) {
+      let selection = event.target.textContent;
+      let playerDamage = 0;
+      let playerHeal = 0;
 
-            let selection = event.target.textContent
-            let playerDamage = 0;
-            let playerHeal = 0;
+      if (selection == "ATTACK") {
+        playerDamage = Math.floor(Math.random() * 10 + 1);
+        this.attackHistory.push(`Player attacks Monster for ${playerDamage}`);
+      } else if (selection == "SPECIAL ATTACK") {
+        playerDamage = Math.floor(Math.random() * 30 + 1);
+        this.attackHistory.push(`Player attacks Monster for ${playerDamage}`);
+      } else if (selection == "HEAL") {
+        playerHeal = Math.floor(Math.random() * 10 + 1);
+        this.attackHistory.push(`Player heals for ${playerHeal}`);
+      }
 
-            if (selection == 'ATTACK') {
-                playerDamage = Math.floor((Math.random() * 10) + 1)
-                this.attackHistory.push(`Player attacks Monster for ${playerDamage}`)
-            }
-            else if (selection =="SPECIAL ATTACK") {
-                playerDamage = Math.floor((Math.random() * 30) + 1)
-                this.attackHistory.push(`Player attacks Monster for ${playerDamage}`)
-            } else if (selection == "HEAL") {
-                playerHeal = Math.floor((Math.random() * 10) + 1);
-                this.attackHistory.push(`Player heals for ${playerHeal}`)
-            }
+      let monsterDamage = Math.floor(Math.random() * 10 + 1);
 
-            let monsterDamage = Math.floor((Math.random() * 10) + 1)
-            
-            this.playerHealth = this.playerHealth - monsterDamage + playerHeal;
-            this.monsterHealth = this.monsterHealth - playerDamage;
-            
-            this.attackHistory.push(`Monster attacks Player for ${monsterDamage}`)
-        },
+      this.playerHealth = this.playerHealth - monsterDamage + playerHeal;
+      this.monsterHealth = this.monsterHealth - playerDamage;
 
-    }
-})
+      this.attackHistory.push(`Monster attacks Player for ${monsterDamage}`);
+    },
+  },
+  computed: {},
+  watch: {
+    playerHealth: function () {
+      if (this.playerHealth <= 0) {
+        return alert("GAME OVER");
+      }
+    },
+    monsterHealth: function () {
+      if (this.monsterHealth <= 0) {
+        return alert("GAME OVER");
+      }
+    },
+  },
+});
