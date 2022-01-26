507e46dc71e26f053801ebdc65f3ec9a9648c36c
ggroshansii <garthgroshansii@gmail.com>
2022-01-26 11:34:56 -0500

    condensed attack, special-attack and heal method into one (using DRY)" git push

--- app.js
+++ app.js
@@ -10,35 +10,31 @@ new Vue({
         startGame: function() {
             this.gameOn = true;
         },
-        attack: function() {
-            let playerDamage = Math.floor((Math.random() * 10) + 1)
-            let monsterDamage = Math.floor((Math.random() * 10) + 1)
-            
-            this.playerHealth = this.playerHealth - monsterDamage;
-            this.monsterHealth = this.monsterHealth - playerDamage;
-            
-            this.attackHistory.push(`Player attacks Monster for ${playerDamage}`)
-            this.attackHistory.push(`Monster attacks Player for ${monsterDamage}`)
-        },
-        specialAttack: function() {
-            let playerDamage = Math.floor((Math.random() * 30) + 1)
-            let monsterDamage = Math.floor((Math.random() * 10) + 1)
-            
-            this.playerHealth = this.playerHealth - monsterDamage;
-            this.monsterHealth = this.monsterHealth - playerDamage;
-            
-            this.attackHistory.push(`Player attacks Monster for ${playerDamage}`)
-            this.attackHistory.push(`Monster attacks Player for ${monsterDamage}`)
-        },
-        heal: function() {
-            let playerHeal = Math.floor((Math.random() * 10) + 1)
-            let monsterDamage = Math.floor((Math.random() * 10) + 1)
-            
-            this.playerHealth = this.playerHealth + playerHeal - monsterDamage;
+        selectionPick: function(event) {
 
+            let selection = event.target.textContent
+            let playerDamage = 0;
+            let playerHeal = 0;
+
+            if (selection == 'ATTACK') {
+                playerDamage = Math.floor((Math.random() * 10) + 1)
+                this.attackHistory.push(`Player attacks Monster for ${playerDamage}`)
+            }
+            else if (selection =="SPECIAL ATTACK") {
+                playerDamage = Math.floor((Math.random() * 30) + 1)
+                this.attackHistory.push(`Player attacks Monster for ${playerDamage}`)
+            } else if (selection == "HEAL") {
+                playerHeal = Math.floor((Math.random() * 10) + 1);
+                this.attackHistory.push(`Player heals for ${playerHeal}`)
+            }
+
+            let monsterDamage = Math.floor((Math.random() * 10) + 1)
             
-            this.attackHistory.push(`Player heals for ${playerHeal}`)
+            this.playerHealth = this.playerHealth - monsterDamage + playerHeal;
+            this.monsterHealth = this.monsterHealth - playerDamage;
+            
             this.attackHistory.push(`Monster attacks Player for ${monsterDamage}`)
-        }
+        },
+
     }
 })
